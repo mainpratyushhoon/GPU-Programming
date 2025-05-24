e157a22dc11db96777d7decdd2c696c88adbc5cd
mainpratyushhoon <mainpratyushoon@gmail.com>
2025-05-24 17:12:56 +0530

    ctime added

--- main.cu
+++ main.cu
@@ -1,9 +1,20 @@
 #include <iostream>
+#include<ctime>
+using namespace std;
 __global__ void helloFromGPU() {
     printf("Hello from GPU!\n");
 }
 int main() {
+    cudaEvent_t start,stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+    cudaEventRecord(start,0);
     helloFromGPU<<<1, 1>>>();
+    cudaEventRecord(stop,0);
+    cudaEventSynchronize(stop);
+    float ms=0;
+    cudaEventElapsedTime(&ms,start,stop);
+    cout<<ms;
     cudaDeviceSynchronize();
     return 0;
 }
