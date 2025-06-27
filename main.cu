f98a61bbfbfa8263e32b56e2939d55039015bf16
mainpratyushhoon <mainpratyushoon@gmail.com>
2025-06-27 17:20:09 +0530

    Python Script added. Graphs added.

--- main.cu
+++ main.cu
@@ -1,31 +1,29 @@
 #include <iostream>
 #include<ctime>
-#define N 1024
-
+#include<chrono>
 using namespace std;
-__global__ void multiplyGPU(int *a,int* b,int *c) {
+int N;
+__global__ void multiplyGPU(int *a,int* b,int *c, int N) {
     int i=blockIdx.x*blockDim.x+threadIdx.x;
     int j=blockIdx.y*blockDim.y+threadIdx.y;
     if(i<N&&j<N){
         int sum=0;
-    for(int k=0;k<N;k++){
-        sum+=a[i*N+k]*b[k*N+j];
+        for(int k=0;k<N;k++){
+            sum+=a[i*N+k]*b[k*N+j];
+        }
+        c[i*N+j]=sum;
     }
-    c[i*N+j]=sum;
-}
-
 }
 int random(){
-    
     int randomNumber = rand() % 10 + 1;
     return randomNumber;
 }
-void assignMatrix(int A[N*N]){
+void assignMatrix(int* A){
     for(int i =0;i<N*N;i++){
         A[i]=random();
     }
 }
-void multiplyCPU(int matrixA[N*N],int matrixB[N*N],int matrixC[N*N]){
+void multiplyCPU(int *matrixA,int* matrixB,int* matrixC){
     for(int i =0;i<N;i++){
         for(int j=0;j<N;j++){
             for(int k=0;k<N;k++){
@@ -34,9 +32,9 @@ void multiplyCPU(int matrixA[N*N],int matrixB[N*N],int matrixC[N*N]){
         };
     }
 }
-void printMatrix(int matrixA[N][N]){
+void printMatrix(int *matrixA){
     for(int i =0;i<N;i++){
-        for(int j=0;j<N;j++) cout<<matrixA[i][j]<<" ";
+        for(int j=0;j<N;j++) cout<<matrixA[i*N+j]<<" ";
         cout<<endl;
     }
     cout<<endl;
@@ -53,9 +51,17 @@ void check(int* a,int* b){
     if (correct) cout << "CPU and GPU results match!" << endl;
     else cout << "CPU and GPU results do NOT match!" << endl;
 }
-int main() {
+
+int main(int argc, char* argv[]){
+
+    if (argc != 2){
+        cout << "Usage: ./main <N>" << endl;
+        return 1;
+    }
+    N = atoi(argv[1]);
+
     //initialization
-    cudaEvent_t start,startG,stop,stopG;
+    cudaEvent_t startG,stopG;
     srand(time(0));
 
     int *A,*B,*C_cpu,*C_gpu;
@@ -71,26 +77,20 @@ int main() {
     memset(C_cpu, 0, sizeof(int) * N * N);
     memset(C_gpu, 0, sizeof(int) * N * N);
 
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start,0);
-
     //matrix calculation using cpu and time measurement 
+    auto cpu_start = chrono::high_resolution_clock::now();
     multiplyCPU(A,B,C_cpu);
     cout<<endl;
+    auto cpu_end = chrono::high_resolution_clock::now();
+    chrono::duration<double> cpu_duration = cpu_end - cpu_start;
     
-    cudaEventRecord(stop,0);
-    cudaEventSynchronize(stop);
-    float cpu=0;
-    cudaEventElapsedTime(&cpu,start,stop);
-
     //matrix calculation using gpu and time measurement
     cudaEventCreate(&startG);
     cudaEventCreate(&stopG);
     cudaEventRecord(startG,0);
 
-    dim3 thread(16,16);
-    dim3 blocks((N +15)/16,(N+15)/16);
+    dim3 thread(2,2);
+    dim3 blocks((N +1)/2,(N+1)/2);
 
     if(cudaMalloc((void**)&d_a,sizeof(int)*N*N)!=cudaSuccess) cout<<"No allocation of A";
     if(cudaMalloc((void**)&d_b,sizeof(int)*N*N)!=cudaSuccess) cout<<"No allocation of B";
@@ -98,7 +98,7 @@ int main() {
     if(cudaMemcpy(d_a,A,sizeof(int)*N*N,cudaMemcpyHostToDevice)!=cudaSuccess) cout<<"No copy of A";
     if(cudaMemcpy(d_b,B,sizeof(int)*N*N,cudaMemcpyHostToDevice)!=cudaSuccess) cout<<"No copy of B";
 
-    multiplyGPU<<<blocks, thread>>>(d_a,d_b,d_c);
+    multiplyGPU<<<blocks, thread>>>(d_a,d_b,d_c,N);
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess) {
         printf("CUDA kernel launch error: %s\n", cudaGetErrorString(err));
@@ -111,7 +111,7 @@ int main() {
     float gpu=0;
     cudaEventElapsedTime(&gpu,startG,stopG);
 
-    cout<<"CPU TIME: "<<cpu<<" ms"<<endl<<"GPU TIME: "<<gpu<<" ms"<<endl;
+    cout << "CPU TIME: " << cpu_duration.count()*1000 << " ms" << endl <<"GPU TIME: "<<gpu<<" ms"<<endl;
     cudaDeviceSynchronize();
 
     check(C_cpu,C_gpu);
