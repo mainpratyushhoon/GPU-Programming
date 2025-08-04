b0216a990e29ca10786f5639ca233a87918996d9
mainpratyushhoon <mainpratyushoon@gmail.com>
2025-08-04 11:37:46 +0530

    New images added

--- graph.py
+++ graph.py
@@ -1,10 +1,10 @@
 import subprocess
 import matplotlib.pyplot as plt
 
-Ns = [i for i in range(1, 100)]
+Ns = [i for i in range(1, 1025)]
 cpu_times = []
 gpu_times = []
-
+speedup=[]
 for N in Ns:
     cpu_time = gpu_time = 0
     for i in range(0,5):
@@ -22,14 +22,14 @@ for N in Ns:
     cpu_time=cpu_time/5
     cpu_times.append(cpu_time)
     gpu_times.append(gpu_time)
-
+    speedup.append(cpu_time/gpu_time)
+#print(speedup)
 # Plotting
 plt.figure(figsize=(10, 6))
-plt.axvline(x=64, color='grey', linestyle='--', label='Reference Line')
-plt.plot(Ns, cpu_times, label="CPU Time", marker="o")
-plt.plot(Ns, gpu_times, label="GPU Time", marker="o")
+plt.axhline(y=1, color='grey', linestyle='--', label='Reference Line')
+plt.plot(Ns, speedup, label="Speed-Up = CPU Time / GPU Time")
 plt.xlabel("Matrix Size (N x N)")
-plt.ylabel("Time (ms)")
+plt.ylabel("SPEED-UP")
 plt.title("CPU vs GPU Matrix Multiplication Time")
 plt.legend()
 plt.grid(True)
